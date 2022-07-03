c46d5c6dfe555f87f95243b05f1a489eb4716bc9
bruhmoment654 <99661323+bruhmoment654@users.noreply.github.com>
2022-07-03 16:18:10 +0300

    1

---
+++ src/test/java/com/company/jmixproject/JmixProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.jmixproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
